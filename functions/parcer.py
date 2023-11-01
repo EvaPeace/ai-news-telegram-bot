363fcdb6fb168ea167f7168553f0b80dbf503a13
YESsPeace <damir200606@gmail.com>
2023-11-01 22:23:57 +0400

    Removed function call in `parcer.py`

--- functions/parcer.py
+++ functions/parcer.py
@@ -1,18 +1,18 @@
 import feedparser
 
-def parse_rss(url):
+
+def parse_rss(url='https://motor.ru/exports/rss'):
     # Создаем объект feedparser для работы с RSS лентой
     feed = feedparser.parse(url)
-    
+
     # Проверяем успешность загрузки RSS ленты
     if feed.status != 200:
         print("Ошибка загрузки RSS ленты")
         return
-    
+
     # Если загрузка RSS ленты прошла успешно, выводим заголовок
-    Titles_list=[]
+    Titles_list = []
     for item in feed.entries:
         Titles_list.append(item.title)
-    Three_First_News=Titles_list[0:3:1]
+    Three_First_News = Titles_list[0:3:1]
     print(Three_First_News)
-parse_rss("https://motor.ru/exports/rss")
