0dbdd5ca2f26fc2cc9c8a8d18866342c0f272034
YESsPeace <damir200606@gmail.com>
2023-11-01 16:56:45 +0400

    Logging added: `base_config` in `config.py` and logger writing in `OpenAI_API.py`

--- functions/OpenAI_API.py
+++ functions/OpenAI_API.py
@@ -1,4 +1,8 @@
+import logging
+
 import openai
+
+logger2 = logging.getLogger(__name__)
 
 
 def get_post_from_ChatGPT(news_headlines: list[str]) -> str:
@@ -12,13 +16,23 @@ def get_post_from_ChatGPT(news_headlines: list[str]) -> str:
     """
 
     user_message = 'Привет, напиши провокационные, но короткие новостные статьи, примерно 100 слов, ' \
-                   'по следующими заголовкам:\n '
+                   'по следующими заголовкам:\n'
 
     user_message = user_message + '\n'.join(news_headlines)
+    message = {"role": "user", "content": user_message}
 
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
-        {"role": "user", "content": user_message}])
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[message])
 
     chatgpt_answer = chat_completion["choices"][0]["message"]["content"]
+
+    logger2.info(f'\nget_post_from_ChatGPT[\n'
+                 f'    "input_data": {news_headlines},\n'
+                 f'    "output_data": {chatgpt_answer}\n'
+                 f']')
+
+    logger2.info(f'\nget_post_from_ChatGPT[\n'
+                 f'    "user_message": "{user_message}",\n'
+                 f'    "chatgpt_answer": {chat_completion}\n'
+                 f']')
 
     return chatgpt_answer
