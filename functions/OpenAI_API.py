3a3db1bfdda17d65c453e82577e1dad8dfa25d0b
YESsPeace <damir200606@gmail.com>
2023-11-01 14:33:05 +0400

    Function `get_post_from_ChatGPT` created

--- functions/OpenAI_API.py
+++ functions/OpenAI_API.py
@@ -10,7 +10,15 @@ def get_post_from_ChatGPT(news_headlines: list[str]) -> str:
     :return: Новостной пост сгенерированный ChatGPT.
     :rtype: str
     """
-    # Код для получения заголовков новостей
-    chatgpt_answer = '...'
+
+    user_message = 'Привет, напиши провокационные, но короткие новостные статьи, примерно 100 слов, ' \
+                   'по следующими заголовкам:\n '
+
+    user_message = user_message + '\n'.join(news_headlines)
+
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+        {"role": "user", "content": user_message}])
+
+    chatgpt_answer = chat_completion["choices"][0]["message"]["content"]
 
     return chatgpt_answer
