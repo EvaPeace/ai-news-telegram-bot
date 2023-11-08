c64ae48e46e5191a7a396e0429577407ac4d5da5
YESsPeace <damir200606@gmail.com>
2023-11-08 21:25:49 +0400

    first ver. of `write_news` function created and tested

--- handlers/channel.py
+++ handlers/channel.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timedelta
-
 from aiogram import types, Dispatcher, Bot
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -15,10 +13,12 @@ class FSMClient(StatesGroup):
 
 async def write_news():
     news_headlines = get_news_headlines()
-    # news_post = get_post_from_ChatGPT(news_headlines)
+    news_post = get_post_from_ChatGPT(news_headlines)
 
-    await bot.send_message(chat_id=channel_id,
-                           text='Для экономии здесь только заголовки:\n' + '\n\n'.join(news_headlines))
+    await bot.send_message(
+        chat_id=channel_id,
+        text='Новости за сегодня:\n' + news_post
+    )
 
 
 def register_handlers_channel(dp: Dispatcher):
@@ -29,12 +29,10 @@ def register_handlers_channel(dp: Dispatcher):
 def start_schedule():
     scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
 
-    scheduler.add_job(write_news, trigger='date', run_date=datetime.now() + timedelta(seconds=10))
+    trigger1 = CronTrigger(hour=7, minute=00, second=0)
+    scheduler.add_job(write_news, trigger=trigger1)
 
-    # trigger1 = CronTrigger(hour=7, minute=00, second=0)
-    # scheduler.add_job(write_news, trigger=trigger1)
-    #
-    # trigger2 = CronTrigger(hour=18, minute=00, second=0)
-    # scheduler.add_job(write_news, trigger=trigger2)
+    trigger2 = CronTrigger(hour=18, minute=00, second=0)
+    scheduler.add_job(write_news, trigger=trigger2)
 
     scheduler.start()
