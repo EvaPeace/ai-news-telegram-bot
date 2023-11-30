9c6637049ad4c8bf02fb87c775c18da78a2023c1
YESsPeace <damir200606@gmail.com>
2023-11-30 15:41:13 +0400

    Added exception handling and logs writing in 'get_news_headlines' function

--- functions/parcer.py
+++ functions/parcer.py
@@ -1,7 +1,13 @@
+import logging
+
 import feedparser
 
+from functions import send_logs_auto
 
-def get_news_headlines(n_news=3, url='https://news.rambler.ru/rss/politics/') -> list[str] | None:
+logger2 = logging.getLogger(__name__)
+
+
+async def get_news_headlines(n_news=3, url='https://news.rambler.ru/rss/politics/') -> list[str] | None:
     """
     Получает список заголовков новостей и возвращает его как список строк (str).
 
@@ -12,21 +18,25 @@ def get_news_headlines(n_news=3, url='https://news.rambler.ru/rss/politics/') ->
     :return: Список заголовков новостей в виде списка строк (str) или None в случае ошибки
     :rtype: list[str] | None
     """
+    try:
+        # Создаем объект feedparser для работы с RSS лентой
+        feed = feedparser.parse(url)
 
-    # Создаем объект feedparser для работы с RSS лентой
-    feed = feedparser.parse(url)
+        # Проверяем успешность загрузки RSS ленты
+        if feed.status != 200:
+            logger2.error('get_news_headlines: Error of loading RSS feed')
+            return
 
-    # Проверяем успешность загрузки RSS ленты
-    if feed.status != 200:
-        print("Ошибка загрузки RSS ленты")
-        return
+        # Если загрузка RSS ленты прошла успешно, выводим заголовок
+        titles_list = []
 
-    # Если загрузка RSS ленты прошла успешно, выводим заголовок
-    titles_list = []
+        for item in feed.entries:
+            titles_list.append(item.title)
 
-    for item in feed.entries:
-        titles_list.append(item.title)
+        three_first_news = titles_list[0:n_news]
 
-    three_first_news = titles_list[0:3:1]
+        return three_first_news
 
-    return three_first_news
+    except Exception as e:
+        logger2.error(f'get_news_headlines: {e}')
+        await send_logs_auto(e)
